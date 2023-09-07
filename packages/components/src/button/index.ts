2848ea2a0cdd441acf3e502bbba1b73f9ca6a45c
张宏强 <hongqiang.zhang@ux168.cn>
2023-09-07 18:32:10 +0800

    commit

--- packages/components/src/button/index.ts
+++ packages/components/src/button/index.ts
@@ -1,4 +1,4 @@
 import _Button from './button.vue';
 import { withInstall } from '@easyest/utils';
-export const Button = withInstall(_Button);
-export default Button;
+export const UxButton = withInstall(_Button);
+export default UxButton;
